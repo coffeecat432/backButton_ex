cb3b58fbdce0be006c8d0d406e7335bd1ed958ea
coffeecat432 <coffeecat432@gmail.com>
2024-06-29 22:06:29 +0900

    back button 한번 누르면 반응, 두번 누르면 종료

--- app/src/main/java/com/amigo/backbutton_ex/MainActivity.kt
+++ app/src/main/java/com/amigo/backbutton_ex/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.amigo.backbutton_ex
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var isDouble = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+    override fun onBackPressed(){
+        Log.d("MainActivity","backbutton")
+        if(isDouble==true){
+            finish()
+        }
+
+        isDouble = true
+        Toast.makeText(this,"종료하실려면 더블클릭", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({
+            isDouble = false
+        },2000)
+
+    }
+
 }
